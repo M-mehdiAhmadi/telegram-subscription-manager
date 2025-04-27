21507fdc51def0aa864079753796c0819cac56c2
mrBabadi <mr.babady1379@gmail.com>
2025-04-27 15:47:13 +0330

    languages updated

--- languages.py
+++ languages.py
@@ -1,24 +1,21 @@
 languages = {
-    "en":{
-        "starthandler":{
-            "message": "Welcome! Please select an option:",
+    "en": {
+        "starthandler": {
+            "text": "Welcome! Please select an option:",
             "keyboard": [
-                { "text": "Subscriptions", "callback_data": "subscriptions" },
-                { "text": "Manage Channels", "callback_data": "manage_channels" },
-                { "text": "Set Language", "callback_data": "set_language" }
-            
-
+                {"text": "Subscriptions", "callback_data": "subscriptions"},
+                {"text": "Manage Channels", "callback_data": "manage_channels"},
+                {"text": "Set Language", "callback_data": "set_language"}
             ]
         }
     },
-    "fa":{
-        "starthandler":{
-            "text":"سلام",
-            "keyboard":[
-                {"text":"","callback_data":""},
-                {"text":"","callback_data":""},
-                {"text":"","callback_data":""}
-
+    "fa": {
+        "starthandler": {
+            "text": "سلام! لطفاً یک گزینه را انتخاب کنید:",
+            "keyboard": [
+                {"text": "اشتراک‌ها", "callback_data": "subscriptions"},
+                {"text": "مدیریت کانال‌ها", "callback_data": "manage_channels"},
+                {"text": "تنظیم زبان", "callback_data": "set_language"}
             ]
         }
     }
